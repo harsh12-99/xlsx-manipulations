e6cfcad1cc37c921c42f927146123ccaf4a941ca
harsh12-99 <malviya.harsh12@gmail.com>
2023-12-06 13:28:47 +0530

    added xlxs file download code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useCallback } from "react";
+import { utils, writeFileXLSX } from "xlsx";
+
+const data = [
+  {
+    name: "TATA",
+    model: "Nexon",
+  },
+  {
+    name: "Maruti",
+    model: "Alto",
+  },
+  {
+    name: "Mahindra",
+    model: "Thar",
+  },
+  {
+    name: "Toyota",
+    model: "Fortuner",
+  },
+];
+
+const Heading = [["Company", "Model"]];
 
 function App() {
+  const exportFile = useCallback(() => {
+    const wb = utils.book_new();
+    const ws = utils.json_to_sheet([]);
+    utils.sheet_add_aoa(ws, Heading);
+
+    utils.sheet_add_json(ws, data, { origin: "A2", skipHeader: true });
+    utils.book_append_sheet(wb, ws, "Sheet1");
+
+    writeFileXLSX(wb, "carsData.xlsx");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Index</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((pres) => (
+          <tr>
+            <td>{pres.name}</td>
+            <td>{pres.model}</td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <td colSpan={2}>
+          <button onClick={exportFile}>Export XLSX</button>
+        </td>
+      </tfoot>
+    </table>
   );
 }
 
